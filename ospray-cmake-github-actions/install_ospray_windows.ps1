66d04231a5f32723d1e6ed945a9ec72869dd7498
WU Qi <wilson.over.cloud@gmail.com>
2022-02-07 22:40:32 -0800

    optix

--- ospray-cmake-github-actions/install_ospray_windows.ps1
+++ ospray-cmake-github-actions/install_ospray_windows.ps1
@@ -2,86 +2,29 @@
 ## Constants
 ## -------------------
 
-# Dictionary of known cuda versions and thier download URLS, which do not follow a consistent pattern :(
+# Dictionary of known ospray versions and thier download URLS, which do not follow a consistent pattern :(
 $OSPRAY_KNOWN_URLS = @{
-    "8.0.44" = "http://developer.nvidia.com/compute/cuda/8.0/Prod/network_installers/cuda_8.0.44_win10_network-exe";
-    "8.0.61" = "http://developer.nvidia.com/compute/cuda/8.0/Prod2/network_installers/cuda_8.0.61_win10_network-exe";
-    "9.0.176" = "http://developer.nvidia.com/compute/cuda/9.0/Prod/network_installers/cuda_9.0.176_win10_network-exe";
-    "9.1.85" = "http://developer.nvidia.com/compute/cuda/9.1/Prod/network_installers/cuda_9.1.85_win10_network";
-    "9.2.148" = "http://developer.nvidia.com/compute/cuda/9.2/Prod2/network_installers2/cuda_9.2.148_win10_network";
-    "10.0.130" = "http://developer.nvidia.com/compute/cuda/10.0/Prod/network_installers/cuda_10.0.130_win10_network";
-    "10.1.105" = "http://developer.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.105_win10_network.exe";
-    "10.1.168" = "http://developer.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.168_win10_network.exe";
-    "10.1.243" = "http://developer.download.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.243_win10_network.exe";
-    "10.2.89" = "http://developer.download.nvidia.com/compute/cuda/10.2/Prod/network_installers/cuda_10.2.89_win10_network.exe";
-    "11.0.1" = "http://developer.download.nvidia.com/compute/cuda/11.0.1/network_installers/cuda_11.0.1_win10_network.exe";
-    "11.0.2" = "http://developer.download.nvidia.com/compute/cuda/11.0.2/network_installers/cuda_11.0.2_win10_network.exe";
-    "11.0.3" = "http://developer.download.nvidia.com/compute/cuda/11.0.3/network_installers/cuda_11.0.3_win10_network.exe";
-    "11.1.0" = "https://developer.download.nvidia.com/compute/cuda/11.1.0/network_installers/cuda_11.1.0_win10_network.exe";
-    "11.1.1" = "https://developer.download.nvidia.com/compute/cuda/11.1.1/network_installers/cuda_11.1.1_win10_network.exe";
-    "11.2.0" = "https://developer.download.nvidia.com/compute/cuda/11.2.0/network_installers/cuda_11.2.0_win10_network.exe";
-    "11.2.1" = "https://developer.download.nvidia.com/compute/cuda/11.2.1/network_installers/cuda_11.2.1_win10_network.exe";
-    "11.2.2" = "https://developer.download.nvidia.com/compute/cuda/11.2.2/network_installers/cuda_11.2.2_win10_network.exe";
-    "11.3.0" = "https://developer.download.nvidia.com/compute/cuda/11.3.0/network_installers/cuda_11.3.0_win10_network.exe";
-    "11.3.1" = "https://developer.download.nvidia.com/compute/cuda/11.3.1/network_installers/cuda_11.3.1_win10_network.exe";
-    "11.5.0" = "https://developer.download.nvidia.com/compute/cuda/11.5.0/network_installers/cuda_11.5.0_win10_network.exe";
-	"11.5.1" = "https://developer.download.nvidia.com/compute/cuda/11.5.1/network_installers/cuda_11.5.1_windows_network.exe"
+    "2.5.0" = "https://github.com/ospray/ospray/releases/download/v2.5.0/ospray-2.5.0.x86_64.windows.zip";
+	"2.6.0" = "https://github.com/ospray/ospray/releases/download/v2.6.0/ospray-2.6.0.x86_64.windows.zip"
 }
 
-# @todo - change this to be based on _MSC_VER intead, or invert it to be CUDA keyed instead?
-$VISUAL_STUDIO_MIN_CUDA = @{
-    "2019" = "10.1";
-    "2017" = "10.0"; # Depends on which version of 2017! 9.0 to 10.0 depending on  version
-    "2015" = "8.0"; # might support older, unsure.
-}
-
-# cuda_runtime.h is in nvcc <= 10.2, but cudart >= 11.0
-# @todo - make this easier to vary per CUDA version.
-$CUDA_PACKAGES_IN = @(
-    "nvcc";
-    "visual_studio_integration";
-    "cublas_dev";
-    "curand_dev";
-    "nvrtc_dev";
-    "cudart";
-)
-
-
 ## -------------------
-## Select CUDA version
+## Select OSPRay version
 ## -------------------
 
-# Get the cuda version from the environment as env:cuda.
-$CUDA_VERSION_FULL = $env:cuda
-# Make sure CUDA_VERSION_FULL is set and valid, otherwise error.
+# Get the ospray version from the environment as env:ospray.
+$OSPRAY_VERSION_FULL = $env:ospray
+# Make sure OSPRAY_VERSION_FULL is set and valid, otherwise error.
 
-# Validate CUDA version, extracting components via regex
-$cuda_ver_matched = $CUDA_VERSION_FULL -match "^(?<major>[1-9][0-9]*)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)$"
-if(-not $cuda_ver_matched){
-    Write-Output "Invalid CUDA version specified, <major>.<minor>.<patch> required. '$CUDA_VERSION_FULL'."
+# Validate OSPRay version, extracting components via regex
+$ospray_ver_matched = $OSPRAY_VERSION_FULL -match "^(?<major>[1-9][0-9]*)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)$"
+if(-not $ospray_ver_matched){
+    Write-Output "Invalid OSPRay version specified, <major>.<minor>.<patch> required. '$OSPRAY_VERSION_FULL'."
     exit 1
 }
-$CUDA_MAJOR=$Matches.major
-$CUDA_MINOR=$Matches.minor
-$CUDA_PATCH=$Matches.patch
-
-## ---------------------------
-## Visual studio support check
-## ---------------------------
-# Exit if visual studio is too new for the cuda version.
-$VISUAL_STUDIO = $env:visual_studio.trim()
-if ($VISUAL_STUDIO.length -ge 4) {
-$VISUAL_STUDIO_YEAR = $VISUAL_STUDIO.Substring($VISUAL_STUDIO.Length-4)
-    if ($VISUAL_STUDIO_YEAR.length -eq 4 -and $VISUAL_STUDIO_MIN_CUDA.containsKey($VISUAL_STUDIO_YEAR)){
-        $MINIMUM_CUDA_VERSION = $VISUAL_STUDIO_MIN_CUDA[$VISUAL_STUDIO_YEAR]
-        if ([version]$CUDA_VERSION_FULL -lt [version]$MINIMUM_CUDA_VERSION) {
-            Write-Output "Error: Visual Studio $($VISUAL_STUDIO_YEAR) requires CUDA >= $($MINIMUM_CUDA_VERSION)"
-            exit 1
-        }
-    }
-} else {
-    Write-Output "Warning: Unknown Visual Studio Version. CUDA version may be insufficient."
-}
+$OSPRAY_MAJOR=$Matches.major
+$OSPRAY_MINOR=$Matches.minor
+$OSPRAY_PATCH=$Matches.patch
 
 ## ------------------------------------------------
 ## Select CUDA packages to install from environment
