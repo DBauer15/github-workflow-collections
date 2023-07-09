25a21125660136a5f92a7d7ae81a11dd9b9e96e1
WU Qi <wilson.over.cloud@gmail.com>
2023-07-08 19:01:49 -0700

    corrections

--- ospray-cmake-github-actions/install_ospray_windows.ps1
+++ ospray-cmake-github-actions/install_ospray_windows.ps1
@@ -27,89 +27,34 @@ $OSPRAY_MINOR=$Matches.minor
 $OSPRAY_PATCH=$Matches.patch
 
 ## ------------------------------------------------
-## Select CUDA packages to install from environment
+## Select OSPRay packages to install from environment
 ## ------------------------------------------------
 
-# $CUDA_PACKAGES = ""
-
-# for CUDA >= 11 cudart is a required package.
-# if([version]$CUDA_VERSION_FULL -ge [version]"11.0") {
-#     if(-not $CUDA_PACKAGES_IN -contains "cudart") {
-#         $CUDA_PACKAGES_IN += 'cudart'
-#     }
-# }
-
-Foreach ($package in $CUDA_PACKAGES_IN) {
-    # Make sure the correct package name is used for nvcc.
-    if($package -eq "nvcc" -and [version]$CUDA_VERSION_FULL -lt [version]"9.1"){
-        $package="compiler"
-    } elseif($package -eq "compiler" -and [version]$CUDA_VERSION_FULL -ge [version]"9.1") {
-        $package="nvcc"
-    }
-    $CUDA_PACKAGES += " $($package)_$($CUDA_MAJOR).$($CUDA_MINOR)"
-
-}
-echo "$($CUDA_PACKAGES)"
-
-## -----------------
-## Prepare download
-## -----------------
-
-# Select the download link if known, otherwise have a guess.
-$CUDA_REPO_PKG_REMOTE=""
-if($OSPRAY_KNOWN_URLS.containsKey($CUDA_VERSION_FULL)){
-    $CUDA_REPO_PKG_REMOTE=$OSPRAY_KNOWN_URLS[$CUDA_VERSION_FULL]
-} else{
-    # Guess what the url is given the most recent pattern (at the time of writing, 10.1)
-    Write-Output "note: URL for CUDA ${$CUDA_VERSION_FULL} not known, estimating."
-    $CUDA_REPO_PKG_REMOTE="http://developer.download.nvidia.com/compute/cuda/$($CUDA_MAJOR).$($CUDA_MINOR)/Prod/network_installers/cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
-}
-$CUDA_REPO_PKG_LOCAL="cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
-
+$RELEASE_URL = $OSPRAY_KNOWN_URLS[$OSPRAY_VERSION_FULL]
+$RELEASE_FILE = "ospray-$OSPRAY_VERSION_FULL.x86_64.windows"
+echo "RELEASE_URL $RELEASE_URL"
+echo "RELEASE_FILE $RELEASE_FILE"
 
 ## ------------
-## Install CUDA
+## Install OSPRay
 ## ------------
 
-# Get CUDA network installer
-Write-Output "Downloading CUDA Network Installer for $($CUDA_VERSION_FULL) from: $($CUDA_REPO_PKG_REMOTE)"
-Invoke-WebRequest $CUDA_REPO_PKG_REMOTE -OutFile $CUDA_REPO_PKG_LOCAL | Out-Null
-if(Test-Path -Path $CUDA_REPO_PKG_LOCAL){
-    Write-Output "Downloading Complete"
-} else {
-    Write-Output "Error: Failed to download $($CUDA_REPO_PKG_LOCAL) from $($CUDA_REPO_PKG_REMOTE)"
-    exit 1
-}
+(New-Object System.Net.WebClient).DownloadFile($RELEASE_URL, "$RELEASE_FILE.zip")
+Expand-Archive -Path "$RELEASE_FILE.zip" -Destination $RELEASE_FILE
 
-# Invoke silent install of CUDA (via network installer)
-Write-Output "Installing CUDA $($CUDA_VERSION_FULL). Subpackages $($CUDA_PACKAGES)"
-Start-Process -Wait -FilePath .\"$($CUDA_REPO_PKG_LOCAL)" -ArgumentList "-s $($CUDA_PACKAGES)"
+## ------------------------------------------------
+## Select OSPRay packages to install from environment
+## ------------------------------------------------
 
-# Check the return status of the CUDA installer.
-if (!$?) {
-    Write-Output "Error: CUDA installer reported error. $($LASTEXITCODE)"
-    exit 1
-}
+$OSPRAY_CMAKE_DIR="$RELEASE_FILE\lib\cmake\ospray-$OSPRAY_VERSION_MAJOR_MINOR"
 
-# Store the CUDA_PATH in the environment for the current session, to be forwarded in the action.
-$CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$($CUDA_MAJOR).$($CUDA_MINOR)"
-$CUDA_PATH_VX_Y = "CUDA_PATH_V$($CUDA_MAJOR)_$($CUDA_MINOR)"
 # Set environmental variables in this session
-$env:CUDA_PATH = "$($CUDA_PATH)"
-$env:CUDA_PATH_VX_Y = "$($CUDA_PATH_VX_Y)"
-Write-Output "CUDA_PATH $($CUDA_PATH)"
-Write-Output "CUDA_PATH_VX_Y $($CUDA_PATH_VX_Y)"
-
-# PATH needs updating elsewhere, anything in here won't persist.
-# Append $CUDA_PATH/bin to path.
-# Set CUDA_PATH as an environmental variable
-
+$env:OSPRAY_CMAKE_DIR = "$($OSPRAY_CMAKE_DIR)"
+Write-Output "OSPRAY_CMAKE_DIR $($OSPRAY_CMAKE_DIR)"
 
 # If executing on github actions, emit the appropriate echo statements to update environment variables
 if (Test-Path "env:GITHUB_ACTIONS") {
-    # Set paths for subsequent steps, using $env:CUDA_PATH
-    echo "Adding CUDA to CUDA_PATH, CUDA_PATH_X_Y and PATH"
-    echo "CUDA_PATH=$env:CUDA_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-    echo "$env:CUDA_PATH_VX_Y=$env:CUDA_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-    echo "$env:CUDA_PATH/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+    # Set paths for subsequent steps, using ${OSPRAY_CMAKE_DIR}
+    echo "Adding OSPRay to OSPRAY_CMAKE_DIR and LD_LIBRARY_PATH"
+    echo "OSPRAY_CMAKE_DIR=$env:OSPRAY_CMAKE_DIR" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 }
