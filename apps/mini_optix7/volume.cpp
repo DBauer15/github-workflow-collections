b1285f5e64c23d62b3c465a6d6942004c63e30f3
WU Qi <wilson.over.cloud@gmail.com>
2023-08-13 21:44:40 -0700

    incorrect use of numeric_limits

--- apps/mini_optix7/volume.cpp
+++ apps/mini_optix7/volume.cpp
@@ -138,7 +138,7 @@ compute_scalar_minmax(void* _array, size_t count, size_t stride)
 
   T init;
 
-  init = std::numeric_limits<T>::min();
+  init = std::numeric_limits<T>::lowest();
   T actual_max = tbb::parallel_reduce(
     tbb::blocked_range<size_t>(0, count), init,
     [value](const tbb::blocked_range<size_t>& r, T v) -> T {
